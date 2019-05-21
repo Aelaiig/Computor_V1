8a95297685c2f595f800a9153ce344e886bdcbaf
Drakauf <shthevak@student.le-101.fr>
2019-05-21 14:15:20 +0200

    before pull ?

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import verbose as verb
 import reader as r
 import globale as g
-
+import simply as s
 import sys
 
 if __name__ == "__main__":
     equation = r.reader(sys.argv)
-    print(equation , g.verbose)
-
+    simply_form = s.simply(equation)
+    print(simply_form)
