04c72cb1c5e4bc823ec0a4c5645985954f4a6318
root <aweiler@z2r2p2.le-101.fr>
2019-05-21 17:05:33 +0200

    regex

--- tools.py
+++ tools.py
@@ -1,27 +1,28 @@
 import re
 
-def simplifier2(item):
-    if item.isnumeric():
-        new_item = item + " * X^0"
-        return new_item
-    elif item == 'X':
-        return item.replace('X', '1 * X^1')
-    elif item[0] == 'X' and item[1] == '^':
-        new_item = "1 * " + item
-        return new_item
-    else :
-        return item
-
 def simplifier(equation):
-    tab = re.split(' ', equation)
-    mapping = list(map(simplifier2, tab))
-    simple = ' '.join(mapping)
-    return simple
+    # simple = ""
+    # j = 0
+    # i = 0
+    # len_equation = len(equation)
+    # while i + 1 < len_equation :
+    #     # if equation[i].isnumeric and i == 0 and equation[i+1] == ' ':
+    #     #     simple[j] = 'X'
+    #     #     simple[j+1] = '^'
+    #     #     simple[j+2] = '0'
+    #     #     i + 1
 
-def ft_ftoa(number):
-    ret = str(number)
-    ln = len(ret)
-    check = ret[ln - 2:]
-    if check == ".0":
-        return ret[:ln - 2]
-    return ret
+    #     # # if equation[i].isnumeric and i != 0 and
+    #     # simple[j] = equation[i]
+    #     print(equation[i])
+    #     i += 1
+    #     j + 1
+    simple = re.sub("(^|[ +=-])(\d+(?:(\.\d+)?))($|[ ][^\*])", r" \2 * X^0\4", equation)
+    simple2 = re.sub("([\+\-\=]|^)\s*[X]([\s*]|$)", r"\1 1 * X^1 ", simple)
+    print('simple' + simple, '\n', simple2, '\n')
+    return simple2
+
+
+    # avant . X^
+    # apres espace
+    #
