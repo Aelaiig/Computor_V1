9e36907475d3422f82311f9dcdf46cfa25c2df32
root <aweiler@z2r2p2.le-101.fr>
2019-05-23 11:33:03 +0200

    simplifier might be ok

--- tools.py
+++ tools.py
@@ -1,28 +1,8 @@
 import re
 
 def simplifier(equation):
-    # simple = ""
-    # j = 0
-    # i = 0
-    # len_equation = len(equation)
-    # while i + 1 < len_equation :
-    #     # if equation[i].isnumeric and i == 0 and equation[i+1] == ' ':
-    #     #     simple[j] = 'X'
-    #     #     simple[j+1] = '^'
-    #     #     simple[j+2] = '0'
-    #     #     i + 1
-
-    #     # # if equation[i].isnumeric and i != 0 and
-    #     # simple[j] = equation[i]
-    #     print(equation[i])
-    #     i += 1
-    #     j + 1
-    simple = re.sub("(^|[ +=-])(\d+(?:(\.\d+)?))($|[ ][^\*])", r" \2 * X^0\4", equation)
-    simple2 = re.sub("([\+\-\=]|^)\s*[X]([\s*]|$)", r"\1 1 * X^1 ", simple)
-    print('simple' + simple, '\n', simple2, '\n')
-    return simple2
-
-
-    # avant . X^
-    # apres espace
-    #
+    simple = re.sub(r"(^|[ +=-])(\d+(?:(\.\d+)?))($|[ ][^\*])", r" \2 * X^0\4", equation)
+    simple2 = re.sub(r"([\+\-\=]|^)\s*[X]([\s*]|$)", r"\1 1 * X^1 ", simple)
+    simple3 = re.sub(r"([^\*]|^)([ ]|^)([X][\^])", r"\1 1 * \3", simple2)
+    # print('simple',  simple, '\n', simple2, '\n', simple3, '\n')
+    return simple3
